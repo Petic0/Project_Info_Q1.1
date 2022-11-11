9c920f90daa326fb03dd69e44d51dd44006d72c9
Peter <peterroemmercollmann@gmail.com>
2022-11-11 21:28:36 +0100

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x = 0;
+        for(int i = 0; i<100;i++) {
+            x++;
+        }
     }
 
 }
